15ae940be60176bcc1589c0079fc852b4a3fc9cd
cvonip <claudiavihester@gmail.com>
2025-06-23 14:30:13 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express     = require('express');
 const bodyParser  = require('body-parser');
 const fccTesting  = require('./freeCodeCamp/fcctesting.js');
+const bcrypt = require('bcrypt');
 const app         = express();
 fccTesting(app);
 const saltRounds = 12;
@@ -10,6 +11,17 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
+bcrypt.hash(myPlaintextPassword, saltRounds, function(err, hash) {
+  if (err) throw err;
+  bcrypt.compare(myPlaintextPassword, hash, function(err, res) {
+    if (err) throw err;
+    // res == true
+  });
+  bcrypt.compare(someOtherPlaintextPassword, hash, function(err, res) {
+    if (err) throw err;
+    // res == false
+  });
+});
 
 
 
